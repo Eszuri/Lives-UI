be7f8cd7f07e29485e4f58ee29915b8c213ad3b9
Eszuri <ualdo296@gmail.com>
2023-12-08 22:15:18 +0700

    we

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import WEll from './components'
+
+export default function App() {
+  return (
+    <>
+      <WEll />
+    </>
+  )
+}
---
+++ src/components/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WEll() {
+    return (
+        <>
+            <h1>WELCOME</h1>
+        </>
+    )
+}
+
+export default WEll
